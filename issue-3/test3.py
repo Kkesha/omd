d66bd9b302daedf32fc80938f9c20962048c013c
Kkesha <67409638+Kkesha@users.noreply.github.com>
2021-11-30 22:51:21 +0300

    Add files via upload

---
+++ issue-3/test3.py
@@ -0,0 +1,74 @@
+from typing import List, Tuple
+import unittest
+
+
+def fit_transform(*args: str) -> List[Tuple[str, List[int]]]:
+    """
+    fit_transform(iterable)
+    fit_transform(arg1, arg2, *args)
+    """
+    if len(args) == 0:
+        raise TypeError('expected at least 1 arguments, got 0')
+
+    categories = args if isinstance(args[0], str) else list(args[0])
+    uniq_categories = set(categories)
+    bin_format = f'{{0:0{len(uniq_categories)}b}}'
+
+    seen_categories = dict()
+    transformed_rows = []
+
+    for cat in categories:
+        bin_view_cat = (int(b) for b in bin_format.format(1 << len(seen_categories)))
+        seen_categories.setdefault(cat, list(bin_view_cat))
+        transformed_rows.append((cat, seen_categories[cat]))
+
+    return transformed_rows
+
+
+class TestOHE(unittest.TestCase):
+    def test_empty(self):
+        """
+        Проверка на пустое значение
+        """
+        test_input = fit_transform([])
+        expected_result = []
+
+        self.assertEqual(test_input, expected_result)
+
+    def test_equal(self):
+        """
+        Проверка равенства
+        """
+        test_input = fit_transform(['Moscow', 'New York', 'Moscow', 'London'])
+        expected_result = [
+            ('Moscow', [0, 0, 1]),
+            ('New York', [0, 1, 0]),
+            ('Moscow', [0, 0, 1]),
+            ('London', [1, 0, 0]),
+        ]
+
+        self.assertEqual(test_input, expected_result)
+
+    def test_exception(self):
+        """
+        Проверка исключения, когда не передаем значений
+        """
+        self.assertRaises(TypeError, fit_transform)
+
+    def test_not_equal(self):
+        """
+        Проверка неравенства
+        """
+        test_input = fit_transform(['Moscow', 'Moscow', 'London'])
+        expected_result = [
+            ('Moscow', [0, 0, 1]),
+            ('New York', [0, 1, 0]),
+            ('Moscow', [0, 0, 1]),
+            ('London', [1, 0, 0]),
+        ]
+
+        self.assertNotEqual(test_input, expected_result)
+
+
+if __name__ == '__main__':
+    unittest.main()
